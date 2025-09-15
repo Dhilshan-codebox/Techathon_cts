f95c4aa9e4ad32e875fb1ceb94005bc4f566ebf6
Divya-Darshan <parukrish.darshan20@gmail.com>
2025-09-15 11:29:56 +0530

    Shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,50 @@
-import Image from "next/image";
-
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from "@/components/ui/hover-card"
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
 export default function Home() {
+
+
+
   return (
     <>
+  <center className="h-screen flex items-center justify-center flex-col gap-10">
+    <HoverCard >
+      <HoverCardTrigger className=" border-b-2 " >Hover <menu type="context"></menu></HoverCardTrigger>
+      <HoverCardContent>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores iure neque aliquid eius blanditiis. Consectetur maiores sint quod, harum, adipisci dignissimos error a natus ipsa maxime, atque non excepturi ipsam?
+      </HoverCardContent>
+    </HoverCard>
+
+        <Popover>
+      <PopoverTrigger>Open</PopoverTrigger>
+      <PopoverContent>Place content for the popover here.</PopoverContent>
+    </Popover>
+    <Select>
+      
+      <SelectTrigger className="w-[180px]">
+        <SelectValue placeholder="Theme" />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectItem value="light">Light</SelectItem>
+        <SelectItem value="dark">Dark</SelectItem>
+        <SelectItem value="system">System</SelectItem>
+      </SelectContent>
+    </Select>
+      </center>
     </>
   );
 }
